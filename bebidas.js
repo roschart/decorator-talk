b6c148400fd33aa3c6f336a1359b5f70e133776d
roschart <jlbalirac@hotmail.com>
2017-08-23 10:12:52 +0200

    Green: Test todas las demas

--- bebidas.js
+++ bebidas.js
@@ -3,7 +3,10 @@ const Bebida = (precio = 0) => ({
   cafe: () => Bebida(1),
   colacao: () => Bebida(1.20),
   conOrujo: () => Bebida(precio + 1),
-  conCanela: () => Bebida(precio + 0.15)
+  conCanela: () => Bebida(precio + 0.15),
+  conSoja: () => Bebida(precio + 0.20),
+  conLeche: () => Bebida(precio + 0.10),
+  conNata: () => Bebida(precio + 0.50)
 })
 
 module.exports.Bebida = Bebida
