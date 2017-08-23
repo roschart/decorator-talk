0df9652a85e0b935358881d19c2e1eebd2b6cb6f
roschart <jlbalirac@hotmail.com>
2017-08-23 10:27:07 +0200

    Refactor: extract constants

--- bebidas.js
+++ bebidas.js
@@ -1,12 +1,21 @@
+const coste = {
+  cafe: 1,
+  colacao: 1.20,
+  orujo: 1,
+  canela: 0.15,
+  soja: 0.20,
+  leche: 0.10,
+  nata: 0.50
+}
 const Bebida = (precio = 0) => ({
   precio: () => precio,
-  cafe: () => Bebida(1),
-  colacao: () => Bebida(1.20),
-  conOrujo: () => Bebida(precio + 1),
-  conCanela: () => Bebida(precio + 0.15),
-  conSoja: () => Bebida(precio + 0.20),
-  conLeche: () => Bebida(precio + 0.10),
-  conNata: () => Bebida(precio + 0.50)
+  cafe: () => Bebida(coste.cafe),
+  colacao: () => Bebida(coste.colacao),
+  conOrujo: () => Bebida(precio + coste.orujo),
+  conCanela: () => Bebida(precio + coste.canela),
+  conSoja: () => Bebida(precio + coste.soja),
+  conLeche: () => Bebida(precio + coste.leche),
+  conNata: () => Bebida(precio + coste.nata)
 })
 
 module.exports.Bebida = Bebida
