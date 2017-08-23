46545ed525edab11ba430cb2599be95496fda83a
roschart <jlbalirac@hotmail.com>
2017-08-23 09:23:10 +0200

    Green: Test Carajillo

--- bebidas.js
+++ bebidas.js
@@ -1,4 +1,7 @@
 exports.Bebida = () => ({
-  cafe: () => ({precio: () => 1}),
+  cafe: () => ({
+    precio: () => 1,
+    conOrujo: () => ({precio: () => 2})
+  }),
   colacao: () => ({precio: () => 1.20})
 })
