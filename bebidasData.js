412f2785c6ce671f2f6b9006d1f30d4bcd9ddae4
Jose Luis Domínguez Balirac <jl.balirac@payvision.com>
2018-06-26 08:32:20 +0200

    Green: Cafe data

--- bebidasData.js
+++ bebidasData.js
@@ -1,5 +1,5 @@
-const coste = {
-  cafe: 1,
+const bebidas = {
+  cafe: {bebida: "cafe", precio: 1},
   colacao: 1.20,
   orujo: 1,
   canela: 0.15,
@@ -8,18 +8,15 @@ const coste = {
   nata: 0.50
 }
 
-const Bebida = precio => ({
-  precio: () => precio,
-  add: aditivo => aditivo(Bebida(precio))
-})
+const Cafe = () => bebidas.cafe
+// const Colacao = () => Bebida(coste.colacao)
 
-const Cafe = () => Bebida(coste.cafe)
-const Colacao = () => Bebida(coste.colacao)
+// const Orujo = bebida => Bebida(bebida.precio() + coste.orujo)
+// const Canela = bebida => Bebida(bebida.precio() + coste.canela)
+// const Soja = bebida => Bebida(bebida.precio() + coste.soja)
+// const Leche = bebida => Bebida(bebida.precio() + coste.leche)
+// const Nata = bebida => Bebida(bebida.precio() + coste.nata)
 
-const Orujo = bebida => Bebida(bebida.precio() + coste.orujo)
-const Canela = bebida => Bebida(bebida.precio() + coste.canela)
-const Soja = bebida => Bebida(bebida.precio() + coste.soja)
-const Leche = bebida => Bebida(bebida.precio() + coste.leche)
-const Nata = bebida => Bebida(bebida.precio() + coste.nata)
+// module.exports = {Cafe, Colacao, Orujo, Canela, Soja, Leche, Nata}
 
-module.exports = {Cafe, Colacao, Orujo, Canela, Soja, Leche, Nata}
+module.exports = {Cafe}
--- test/cafe.data.spec.js
+++ test/cafe.data.spec.js
@@ -1,26 +1,28 @@
 const expect = require('chai').expect
-const {Cafe, Colacao, Orujo, Canela, Soja, Nata, Leche} = require('../bebidasData.js')
-describe('Precios de café y colacao', () => {
-  describe('Bebidas simples', () => {
-    it('Café', () => {
-      expect(Cafe()).to.equal({bebida: "cafe" , precio : 1.0   })
+const { Cafe, Colacao, Orujo, Canela, Soja, Nata, Leche } = require('../bebidasData.js')
+describe('Data oriented design', () => {
+  describe('Precios de café y colacao', () => {
+    describe('Bebidas simples', () => {
+      it('Café', () => {
+        expect(Cafe()).to.deep.equal({ bebida: "cafe", precio: 1.0 })
+      })
+      // it('Colacao', () => {
+      //   expect(Colacao().precio()).to.equal(1.20)
+      // })
     })
-    // it('Colacao', () => {
-    //   expect(Colacao().precio()).to.equal(1.20)
+
+    // describe('Bebidas con complementos', () => {
+    //   it('Carajillo', () => {
+    //     expect(Cafe().add(Orujo).precio()).to.equal(2)
+    //   })
+    //   it('Colacao con canela', () => {
+    //     expect(Colacao().add(Canela).precio()).to.be.closeTo(1.35, 0.001)
+    //   })
+    //   it('Todos los demas', () => {
+    //     expect(Cafe().add(Soja).precio()).to.be.closeTo(1.20, 0.001)
+    //     expect(Cafe().add(Leche).add(Nata).precio()).to.be.closeTo(1.60, 0.001)
+    //     expect(Cafe().add(Leche).add(Leche).add(Nata).add(Orujo).precio()).to.be.closeTo(2.70, 0.001)
+    //   })
     // })
   })
-
-  // describe('Bebidas con complementos', () => {
-  //   it('Carajillo', () => {
-  //     expect(Cafe().add(Orujo).precio()).to.equal(2)
-  //   })
-  //   it('Colacao con canela', () => {
-  //     expect(Colacao().add(Canela).precio()).to.be.closeTo(1.35, 0.001)
-  //   })
-  //   it('Todos los demas', () => {
-  //     expect(Cafe().add(Soja).precio()).to.be.closeTo(1.20, 0.001)
-  //     expect(Cafe().add(Leche).add(Nata).precio()).to.be.closeTo(1.60, 0.001)
-  //     expect(Cafe().add(Leche).add(Leche).add(Nata).add(Orujo).precio()).to.be.closeTo(2.70, 0.001)
-  //   })
-  // })
 })
