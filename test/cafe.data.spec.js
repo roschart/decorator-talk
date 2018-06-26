961487ebb1d0d20310fd140c496d6e699ced9820
Jose Luis Domínguez Balirac <jl.balirac@payvision.com>
2018-06-26 08:39:01 +0200

    Red: Add tests for colacao

--- test/cafe.data.spec.js
+++ test/cafe.data.spec.js
@@ -3,26 +3,32 @@ const { Cafe, Colacao, Orujo, Canela, Soja, Nata, Leche } = require('../bebidasD
 describe('Data oriented design', () => {
   describe('Precios de café y colacao', () => {
     describe('Bebidas simples', () => {
-      it('Café', () => {
+      it('Data object of Café', () => {
         expect(Cafe()).to.deep.equal({ bebida: "cafe", precio: 1.0 })
       })
-      // it('Colacao', () => {
-      //   expect(Colacao().precio()).to.equal(1.20)
+      it('Price Café', () => {
+        expect(Cafe().precio).to.equal(1)
+      })
+      it('Data object of Colacao', () => {
+        expect(Colacao()).to.deep.equal({ bebida: "colacao", precio: 1.2 })
+      })
+      it('Price Colacao', () => {
+        expect(Colacao().precio).to.equal(1.20)
+      })
+
+      // describe('Bebidas con complementos', () => {
+      //   it('Carajillo', () => {
+      //     expect(Cafe().add(Orujo).precio()).to.equal(2)
+      //   })
+      //   it('Colacao con canela', () => {
+      //     expect(Colacao().add(Canela).precio()).to.be.closeTo(1.35, 0.001)
+      //   })
+      //   it('Todos los demas', () => {
+      //     expect(Cafe().add(Soja).precio()).to.be.closeTo(1.20, 0.001)
+      //     expect(Cafe().add(Leche).add(Nata).precio()).to.be.closeTo(1.60, 0.001)
+      //     expect(Cafe().add(Leche).add(Leche).add(Nata).add(Orujo).precio()).to.be.closeTo(2.70, 0.001)
+      //   })
       // })
     })
-
-    // describe('Bebidas con complementos', () => {
-    //   it('Carajillo', () => {
-    //     expect(Cafe().add(Orujo).precio()).to.equal(2)
-    //   })
-    //   it('Colacao con canela', () => {
-    //     expect(Colacao().add(Canela).precio()).to.be.closeTo(1.35, 0.001)
-    //   })
-    //   it('Todos los demas', () => {
-    //     expect(Cafe().add(Soja).precio()).to.be.closeTo(1.20, 0.001)
-    //     expect(Cafe().add(Leche).add(Nata).precio()).to.be.closeTo(1.60, 0.001)
-    //     expect(Cafe().add(Leche).add(Leche).add(Nata).add(Orujo).precio()).to.be.closeTo(2.70, 0.001)
-    //   })
-    // })
   })
 })
